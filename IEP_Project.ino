37fa3d49fff32f59d355014f1008c83cfe425330
A-Double-M <aungmyintam69.24@ichat.sp.edu.sg>
2024-07-22 12:07:03 +0800

    Commented File
    Add some comments

--- IEP_Project.ino
+++ IEP_Project.ino
@@ -1,24 +1,30 @@
+// Header files
 #include "RichShieldTM1637.h"
 
+// Define Macro
 #define BUTTONK1 8
 #define BUTTONK2 9
 #define KNOB_PIN A0
 #define CLK 10
 #define DIO 11
 
+// Global variable
 int knobValue;
 int knobArr[3];
 int K1Counter;
 int average;
 
+// 
 TM1637 disp(CLK,DIO);
 
-void mainBlock();
+// Function declaration
+// void mainBlock();
 void getValue();
 int getAverage();
 void setK1Counter();
 void clearDisplay();
 
+// Setup function
 void setup(){
   pinMode(BUTTONK2, INPUT_PULLUP);
   pinMode(BUTTONK1, INPUT_PULLUP);
@@ -26,20 +32,19 @@ void setup(){
   disp.init();
 }
 
+// Loop function
 void loop(){
   if(digitalRead(BUTTONK2) == 0){
     clearDisplay();
     while(digitalRead(BUTTONK2) == 0);
-    mainBlock();
+    //mainBlock();
+    getValue();
+    average = getAverage();
+    disp.display(average);
   }
 }
 
-void mainBlock(){
-  getValue();
-  average = getAverage();
-  disp.display(average);
-}
-
+// Getting value from petitometer
 void getValue(){
   K1Counter = 0;
   
@@ -61,6 +66,7 @@ void getValue(){
   }
 }
 
+// Calculating average value from three value of petitometer
 int getAverage(){
   int sum, avg;
   for(int i=0; i<3; i++)
@@ -69,6 +75,7 @@ int getAverage(){
   return avg;
 }
 
+// Button K1, opening the application and restarting it
 void setK1Counter(){
   if(digitalRead(BUTTONK1) == 0){
     if(!K1Counter) K1Counter = 1;
@@ -77,6 +84,7 @@ void setK1Counter(){
   while(digitalRead(BUTTONK1) == 0);
 }
 
+// Clearing out display in 4 digit segment
 void clearDisplay(){
   for(int i=0; i<4; i++)
     disp.display(i, 16);
